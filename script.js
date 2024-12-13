c9931fcb5e1374600a4176a1f10fa5f68df48e99
Qahhorali <qahhorali04@gmail.com>
2024-12-13 17:45:29 +0500

    jsga ozgartirish kiritdim

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const btn = document.querySelector('.hamburger');
 const logo = document.getElementById('logoName');
+const saidbar = document.getElementById('saidbar');
 const nav = document.getElementById('nav');
 btn.addEventListener('click', () => {
-    const saidbar = document.getElementById('saidbar');
-    saidbar.style.width = "86px";
-    logo.style.display = "none";
-    nav.style.display = "none";
+    if (logo.style.display == 'flex'){
+        saidbar.style.width = "86px";
+        logo.style.display = "none";
+        nav.style.display = "none";
+    }else{
+        saidbar.style.width = "300px";
+        logo.style.display = "flex";
+        nav.style.display = "block";
+    }
 })
-console.log(btn);
+
+
